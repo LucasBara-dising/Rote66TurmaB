65df46fa9263f9de9872328799cb772c640e114d
LucasBara-dising <lucas.Barauna098@gmail.com>
2022-05-16 17:28:10 -0300

    Add project files.

---
+++ Rote66TurmaB/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Rote66TurmaB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rote66TurmaB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEnumerable<Noticia> AsNoticias;
+            
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Rote66TurmaB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rote66TurmaB.MvcApplication" Language="C#" %>
